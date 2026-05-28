2ecef0710810dab2d61a800f5eb521e67da3feaf
Ulrich Mathes <mathes@sitegeist.de>
2026-05-28 10:07:11 +0200

    [TASK] set TYPO3 LTS as required version

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,8 +12,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0-14.9.99',
-            'php' => '7.4.0-8.9.99'
+            'typo3' => '14.3.0-14.9.99',
+            'php' => '8.2.0-8.9.99'
         ],
         'conflicts' => [
         ],
